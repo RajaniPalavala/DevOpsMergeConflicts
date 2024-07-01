2a4c4917d12dec5f5035e17e2d4f77652aa7227f
rajani.palavala <rajani.palavala@gmail.com>
2024-07-01 22:47:39 +0530

    code missed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Sum of user input is "+sumOf(5,7));
+        System.out.println("Sum of user input is "+sumOf(5,7,3));
     }
 
     /**
