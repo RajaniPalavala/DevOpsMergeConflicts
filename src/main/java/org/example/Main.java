7fe77986e23d11b62b3b4fa2477af38fe4130630
rajani.palavala <rajani.palavala@gmail.com>
2024-07-01 18:35:35 +0530

    first modification commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,10 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Sum of user input is "+sumOf(5,7));
+    }
+
+    public static int sumOf(int a, int b){
+        return a + b;
     }
 }
