dd92fb3fe2a9edc7255f6c1a608484a918785400
rajani.palavala <rajani.palavala@gmail.com>
2024-07-01 22:00:40 +0530

    This merge conflict commit from Dev1 branch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,10 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Sum of user input is "+sumOf(5,7));
+        System.out.println("Sum of user input is "+sumOf(5,7, 3));
     }
 
-    public static int sumOf(int a, int b){
+    public static int sumOf(int a, int b, int c){
         return a + b;
     }
 }
