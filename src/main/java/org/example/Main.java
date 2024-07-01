ce5a742d05ab03a515853a31a002beba8820fa42
rajani.palavala <rajani.palavala@gmail.com>
2024-07-01 21:46:08 +0530

    This merge conflict commit 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,19 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Sum of user input is "+sumOf(5,7));
+        System.out.println("Sum of user input is "+sumOf(5,7, 3));
     }
 
-    public static int sumOf(int a, int b){
-        return a + b;
+    /**
+     * Calculates the sum of three integers.
+     *
+     * @param a the first integer
+     * @param b the second integer
+     * @param c the third integer
+     * @return the sum of {@code a}, {@code b}, and {@code c}
+     */
+    public static int sumOf(int a, int b, int c){
+        int d = a + b + c;
+        return d;
     }
 }
